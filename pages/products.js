fe4224ae30297a013de58fecea27aaa1875b6aec
NullDefault <davisha999@gmail.com>
2022-04-18 14:48:57 -0400

    cleaned up product page

--- pages/products.js
+++ pages/products.js
@@ -20,60 +20,92 @@ function GridItemWrapper({ children }) {
   );
 }
 
+function ColumnItemWrapper({ children }) {
+  return (
+    <Container w="100%" h="400px">
+      {children}
+    </Container>
+  );
+}
+
+function TextDescription({ title, body }) {
+  return (
+    <Flex direction="column" justify="center" h="full">
+      <chakra.h1 fontSize={48} fontWeight={"bold"}>
+        {title}
+      </chakra.h1>
+      {body}
+    </Flex>
+  );
+}
+
 export default function Products() {
+  const renderAsColumn = useBreakpointValue({ base: true, md: false });
+
+  const dumbDogText = (
+    <TextDescription
+      title="Dumb Dog"
+      body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+est laborum."
+    />
+  );
+  const dumbDogModel = (
+    <ModelViewer
+      model="dumbdog"
+      scale={useBreakpointValue({ base: 0.1, lg: 0.15 })}
+      position={[0, -12, 0]}
+      rotationAxes="y"
+    />
+  );
+
+  const longDogText = (
+    <TextDescription
+      title="Long Dog"
+      body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+  est laborum."
+    />
+  );
+  const longDogModel = (
+    <ModelViewer
+      model="longdog"
+      scale={useBreakpointValue({ base: 0.6, lg: 1 })}
+      rotation={[1.5, 0, 0]}
+      rotationAxes="y"
+    />
+  );
+
   return (
     <>
       <ProductsHeader />
       <Box minH="60vh">
-        <Grid
-          templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
-          gap={6}
-        >
-          <GridItemWrapper>
-            <ModelViewer
-              model="dumbdog"
-              scale={useBreakpointValue({ base: 0.1, lg: 0.15 })}
-              position={[0, -12, 0]}
-              rotationAxes="y"
-            />
-          </GridItemWrapper>
-          <GridItemWrapper>
-            <Flex direction="column" justify="center" h="full">
-              <chakra.h1 fontSize={48} fontWeight={"bold"}>
-                Dumb Dog
-              </chakra.h1>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Flex>
-          </GridItemWrapper>
-          <GridItemWrapper>
-            <Flex direction="column" justify="center" h="full">
-              <chakra.h1 fontSize={48} fontWeight={"bold"}>
-                Long Dog
-              </chakra.h1>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Flex>
-          </GridItemWrapper>
-          <GridItemWrapper>
-            <ModelViewer
-              model="longdog"
-              scale={useBreakpointValue({ base: 0.6, lg: 1 })}
-              rotation={[1.5, 0, 0]}
-              rotationAxes="y"
-            />
-          </GridItemWrapper>
-        </Grid>
+        {renderAsColumn ? (
+          <Flex direction="column" py={10}>
+            <ColumnItemWrapper>{dumbDogModel}</ColumnItemWrapper>
+            <ColumnItemWrapper>{dumbDogText}</ColumnItemWrapper>
+            <ColumnItemWrapper>{longDogModel}</ColumnItemWrapper>
+            <ColumnItemWrapper>{longDogText}</ColumnItemWrapper>
+          </Flex>
+        ) : (
+          <Grid
+            templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
+            gap={6}
+          >
+            <GridItemWrapper>{dumbDogModel}</GridItemWrapper>
+            <GridItemWrapper>{dumbDogText}</GridItemWrapper>
+            <GridItemWrapper>{longDogText}</GridItemWrapper>
+            <GridItemWrapper>{longDogModel}</GridItemWrapper>
+          </Grid>
+        )}
       </Box>
     </>
   );
