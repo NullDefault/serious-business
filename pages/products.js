e84735da8a214aace5ea60dda5f2f71e58152dc5
NullDefault <davisha999@gmail.com>
2022-04-19 12:16:33 -0400

    cleaning up product page

--- pages/products.js
+++ pages/products.js
@@ -42,47 +42,52 @@ function TextDescription({ title, body }) {
 export default function Products() {
   const renderAsColumn = useBreakpointValue({ base: true, md: false });
 
-  const dumbDogText = (
-    <TextDescription
-      title="Dumb Dog"
-      body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-est laborum."
-    />
-  );
-  const dumbDogModel = (
-    <ModelViewer
-      model="dumbdog"
-      scale={useBreakpointValue({ base: 0.1, lg: 0.15 })}
-      position={[0, -12, 0]}
-      rotationAxes="y"
-    />
-  );
-
-  const longDogText = (
-    <TextDescription
-      title="Long Dog"
-      body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  const dogData = [
+    {
+      text: (
+        <TextDescription
+          title="Dumb Dog"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
   veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
   commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
   velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
   cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
   est laborum."
-    />
-  );
-  const longDogModel = (
-    <ModelViewer
-      model="longdog"
-      scale={useBreakpointValue({ base: 0.6, lg: 1 })}
-      rotation={[1.5, 0, 0]}
-      rotationAxes="y"
-    />
-  );
+        />
+      ),
+      model: (
+        <ModelViewer
+          model="dumbdog"
+          scale={useBreakpointValue({ base: 0.1, lg: 0.15 })}
+          position={[0, -12, 0]}
+          rotationAxes="y"
+        />
+      ),
+    },
+    {
+      text: (
+        <TextDescription
+          title="Long Dog"
+          body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+  veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+  commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+  velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+  cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+  est laborum."
+        />
+      ),
+      model: (
+        <ModelViewer
+          model="longdog"
+          scale={useBreakpointValue({ base: 0.6, lg: 1 })}
+          rotation={[1.5, 0, 0]}
+          rotationAxes="y"
+        />
+      ),
+    },
+  ];
 
   return (
     <>
@@ -90,20 +95,41 @@ est laborum."
       <Box minH="60vh">
         {renderAsColumn ? (
           <Flex direction="column" py={10}>
-            <ColumnItemWrapper>{dumbDogModel}</ColumnItemWrapper>
-            <ColumnItemWrapper>{dumbDogText}</ColumnItemWrapper>
-            <ColumnItemWrapper>{longDogModel}</ColumnItemWrapper>
-            <ColumnItemWrapper>{longDogText}</ColumnItemWrapper>
+            {dogData.map((dog, index) => {
+              return [
+                <ColumnItemWrapper key={"dog" + { index } + "-model"}>
+                  {dog.model}
+                </ColumnItemWrapper>,
+                <ColumnItemWrapper key={"dog" + { index } + "-text"}>
+                  {dog.text}
+                </ColumnItemWrapper>,
+              ];
+            })}
           </Flex>
         ) : (
           <Grid
             templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(2, 1fr)" }}
             gap={6}
           >
-            <GridItemWrapper>{dumbDogModel}</GridItemWrapper>
-            <GridItemWrapper>{dumbDogText}</GridItemWrapper>
-            <GridItemWrapper>{longDogText}</GridItemWrapper>
-            <GridItemWrapper>{longDogModel}</GridItemWrapper>
+            {dogData.map((dog, index) => {
+              return index / 2 == 0
+                ? [
+                    <GridItemWrapper key={"dog" + { index } + "-model"}>
+                      {dog.model}
+                    </GridItemWrapper>,
+                    <GridItemWrapper key={"dog" + { index } + "-text"}>
+                      {dog.text}
+                    </GridItemWrapper>,
+                  ]
+                : [
+                    <GridItemWrapper key={"dog" + { index } + "-text"}>
+                      {dog.text}
+                    </GridItemWrapper>,
+                    <GridItemWrapper key={"dog" + { index } + "-model"}>
+                      {dog.model}
+                    </GridItemWrapper>,
+                  ];
+            })}
           </Grid>
         )}
       </Box>
