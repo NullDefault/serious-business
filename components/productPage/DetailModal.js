3322e011ebf7f0f0f6e1438a4b5b5f41531bd141
NullDefault <davisha999@gmail.com>
2022-04-21 17:55:27 -0400

    more fleshing out of the product modal

--- components/productPage/DetailModal.js
+++ components/productPage/DetailModal.js
@@ -11,38 +11,61 @@ import {
   Text,
   Box,
 } from "@chakra-ui/react";
+import Carousel from "../Carousel";
 
 export default function DetailModal(props) {
+  let slides = [
+    <Flex>
+      <Image
+        boxSize="xl"
+        w="70%"
+        pr={4}
+        src="https://www.treehugger.com/thmb/XuoUZ9hFh08TtrXVJ0iPNNwLmow=/1000x562/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__03__senior_man_walking_dog-adf3819af0cb4a3f93a0cea5387fd5e6.jpg"
+      />
+      <Text w="30%">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+      </Text>
+    </Flex>,
+    <Flex>
+      <Image
+        boxSize="xl"
+        w="70%"
+        pr={4}
+        src="https://www.treehugger.com/thmb/XuoUZ9hFh08TtrXVJ0iPNNwLmow=/1000x562/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__03__senior_man_walking_dog-adf3819af0cb4a3f93a0cea5387fd5e6.jpg"
+      />
+      <Text w="35%">
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+        accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab
+        illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+        explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut
+        odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum
+        quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam
+        eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+        voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam
+        corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur?
+      </Text>
+    </Flex>,
+  ];
   return (
-    <Modal size="6xl" isOpen={props.isOpen} onClose={props.onClose}>
+    <Modal size="" isOpen={props.isOpen} onClose={props.onClose}>
       <ModalOverlay />
-      <ModalContent my="auto">
+      <ModalContent my="auto" mx="10vw">
         <ModalHeader>
           <Box h="3px" bg="blackAlpha.400" />
         </ModalHeader>
-        <ModalCloseButton m={4} />
+        <ModalCloseButton m={2} />
         <ModalBody>
-          <Flex>
-            <Image
-              h="50%"
-              w="60%"
-              pr={4}
-              src="https://www.treehugger.com/thmb/XuoUZ9hFh08TtrXVJ0iPNNwLmow=/1000x562/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2019__03__senior_man_walking_dog-adf3819af0cb4a3f93a0cea5387fd5e6.jpg"
-            />
-            <Text w="35%">
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam
-              est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci
-              velit, sed quia non numquam eius modi tempora incidunt ut labore
-              et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima
-              veniam, quis nostrum exercitationem ullam corporis suscipit
-              laboriosam, nisi ut aliquid ex ea commodi consequatur?
-            </Text>
-          </Flex>
+          <Carousel slides={slides} />
         </ModalBody>
 
         <ModalFooter>
